db9ce7582b86291cf89b716f052e31566780f15b
Junnyldo Costa <junnyldo.costa@raltecbr.com>
2024-02-12 23:15:22 -0300

    infocase em projeto CodeIgniter 4

---
+++ public/index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Réplica Layout</title>
+        <link rel="icon" href="img/icon.png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+        <style>
+            .carousel-item img.d-block {
+                filter: brightness(40%); /* Escurece as imagens */
+            }
+
+            #anim-texto-cima-baixo1-slide1 {
+                position: absolute;
+                top: -100%; /* Posiciona o texto fora do slide acima */
+                left: 40%;
+                transform: translateX(-50%);
+                color: white; /* Cor do texto */
+                transition: top 1.5s ease; /* Adiciona transição para animação */
+                font-size: 70px;
+            }
+            .carousel-item.active #anim-texto-cima-baixo1-slide1,
+            .carousel-item.anim-texto-cima-baixo1-slide1 {
+                top: 15%; /* Move o texto para a posição inicial */
+            }
+
+            #anim-texto-cima-baixo-slide3 {
+                position: absolute;
+                top: -100%; /* Ajuste a posição conforme necessário */
+                left: 40%;
+                transform: translateX(-50%);
+                color: white; /* Cor do texto */
+                transition: top 1.5s ease; /* Adiciona transição para animação */
+                font-size: 60px;
+            }
+            .carousel-item.active #anim-texto-cima-baixo-slide3,
+            .carousel-item.anim-texto-cima-baixo-slide3 {
+                top: 2%; /* Move o texto para a posição inicial */
+            }
+
+            /* Para o parágrafo do slide 3 */
+            #anim-p-cima-baixo-slide3 {
+                position: absolute;
+                top: -100%; /* Posiciona o texto fora do slide acima */
+                left: 50%; /* Ajuste conforme necessário */
+                transform: translate(-50%, 0);
+                color: white; /* Cor do texto */
+                transition: top 1.5s ease; /* Adiciona transição para animação */
+                font-size: 25px; /* Tamanho da fonte */
+                opacity: 0; /* Esconde o texto inicialmente */
+            }
+
+            .carousel-item.active #anim-p-cima-baixo-slide3,
+            .carousel-item.anim-p-cima-baixo-slide3 {
+                top: 40%; /* Move o texto para a posição inicial */
+                opacity: 1; /* Mostra o texto */
+            }
+
+            .anim-right-left {
+                position: absolute;
+                top: 0;
+                padding-left: 10%;
+                right: -100%; /* Posiciona a imagem secundária fora do slide à esquerda */
+                transition: right 0.5s ease; /* Adiciona transição para animação */
+            }
+            .carousel-item.active .anim-right-left {
+                right: 0; /* Move a imagem secundária para a posição inicial quando o slide estiver ativo */
+                margin-right: 20px;
+            }            
+            .carousel-item.active .anim-right-left, /* Aplica a animação apenas quando o slide estiver ativo */
+            .carousel-item.anim-right-left {
+                right: 0; /* Move a imagem secundária para a posição inicial */
+            }
+            .right-left-p-text {
+                position: absolute;
+                top: 70%;
+                left: 100%; /* Inicia o texto à direita */
+                transform: translate(-80%, -50%);
+                transition: left 1.5s ease; /* Adiciona a transição */
+                font-size: 25px;
+                opacity: 0; /* Esconde o texto inicialmente */
+            }
+
+            .right-left-p-text.slide-in {
+                left: 50%; /* Move o texto para a posição correta */
+                opacity: 1; /* Mostra o texto */
+            }
+            .right-left-p-text2 {
+                position: absolute;
+                top: 30%;
+                left: 100%; /* Inicia o texto à direita */
+                transform: translate(-80%, -50%);
+                transition: left 1.5s ease; /* Adiciona a transição */
+                font-size: 25px;
+                opacity: 0; /* Esconde o texto inicialmente */
+            }
+
+            .right-left-p-text2.slide-in {
+                left: 50%; /* Move o texto para a posição correta */
+                opacity: 1; /* Mostra o texto */
+            }
+
+            .facebook-icon, .twitter-icon, .google-icon, .linkedin-icon {
+                font-size: 1.2em; /* Ajuste o tamanho dos ícones conforme necessário */
+                margin-right: 1em; /* Adicione espaço entre os ícones */
+            }
+
+            #secondary-img1 {
+                position: absolute; 
+                height: 90%;
+                padding-top: 2%;
+                padding-right: 15%;
+            }
+
+            #secondary-img2 {
+                position: absolute; 
+                height: 68%;
+                padding-top: 0;
+                padding-right: 15%;
+            }
+
+            #anim-left-right-slide2 {
+                position: absolute;
+                left: -100%; /* Inicia o texto à direita */
+                top: 30%; /* Ajuste conforme necessário */
+                transform: translate(0, -50%);
+                transition: left 1.5s ease; /* Adiciona a transição */
+                font-size: 65px;
+                opacity: 0; /* Esconde o texto inicialmente */
+            }
+
+            .carousel-item.active #anim-left-right-slide2.slide-in {
+                left: 5%; /* Move o texto para a posição correta */
+                opacity: 1; /* Mostra o texto */
+            }
+            
+            #right-left-p-text-slide2 {
+                position: absolute;
+                top: 60%; /* Ajuste conforme necessário */
+                left: 100%; /* Inicia o texto à direita */
+                transform: translate(-80%, -50%);
+                transition: left 1.5s ease; /* Adiciona a transição */
+                font-size: 25px;
+                opacity: 0; /* Esconde o texto inicialmente */
+            }
+
+            #right-left-p-text-slide2.slide-in {
+                left: 50%; /* Move o texto para a posição correta */
+                opacity: 1; /* Mostra o texto */
+            }
+
+            .custom-placeholder {
+                font-size: 16px;
+                font-weight: bold;
+                line-height: 3;
+            }
+
+            /* Para o parágrafo do slide 3 */
+            #right-left-p-text-slide3 {
+                position: absolute;
+                top: 55%;
+                left: 100%; /* Inicia o texto à direita */
+                transform: translate(-80%, -50%);
+                transition: left 1.5s ease; /* Adiciona a transição */
+                font-size: 25px; /* Tamanho da fonte */
+                opacity: 0; /* Esconde o texto inicialmente */
+            }
+
+            #right-left-p-text-slide3.slide-in {
+                left: 50%; /* Move o texto para a posição correta */
+                opacity: 1; /* Mostra o texto */
+            }
+
+        </style>
+
+    </head>
+
+    <body>
+        
+        <header class="p-2 text-bg-warning">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                    </a>
+        
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li class="nav-item d-flex align-items-center">
+                            <i class="fas fa-envelope me-1"></i>
+                            <a href="mailto:email@email.com.br" class="nav-link text-dark fs-4 text">email@email.com.br</a>
+                        </li>
+                    </ul>
+        
+                    <ul class="nav col-12 col-lg-auto ms-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li class="nav-item">
+                            <i class="fab fa-facebook-square facebook-icon"></i>
+                            <i class="fab fa-twitter-square twitter-icon"></i>
+                            <i class="fab fa-google-plus-square google-icon"></i>
+                            <i class="fab fa-linkedin linkedin-icon"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>   
+
+        <div id="menuNav" class="container-fluid bg-black py-2 px-2">
+            <div class="container px-5">
+                <div class="row align-items-center">
+                    <div class="col-lg-4">
+                        <a href="index.html"><img src="img/logo.png" alt="logo"></a>
+                    </div>
+                    <div class="col-lg-8">
+                        <nav class="navbar navbar-expand-lg navbar-dark">
+                            <div class="container-fluid">
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#"><h4 class="text-light">Início</h4></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#"><h4 class="text-light">Sobre</h4></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#"><h4 class="text-light">Jogadores</h4></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#"><h4 class="text-light">Galeria</h4></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#"><h4 class="text-light">Mídia Kit</h4></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#"><h4 class="text-light">Contato</h4></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Slider -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/sld1.png" class="d-block w-100" alt="imagem1">
+                    <img src="img/player.png" class="d-none d-xl-block anim-right-left" id="secondary-img1" alt="imagem3_secundaria">
+                    <h1 class="text-capitalize" id="anim-texto-cima-baixo1-slide1">Uma jogada bem agenciada pode mudar sua história</h1>
+                    <p class="right-left-p-text text-light pt-5 d-none d-xl-block">Gerenciamos carreiras de craques do futebol: Da base ao profissional, do primeiro chute ao gol da vitória. Com o comprometimento que o seu sonho merece.</p>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/sld2.png" class="d-block w-100" alt="imagem2">
+                    <h1 id="anim-left-right-slide2" class="text-capitalize text-light">Criando oportunidades e soluções <br> para atletas</h1>
+                    <p id="right-left-p-text-slide2" class="right-left-p-text text-light pt-5 d-none d-xl-block slide-in">É comprovado: Atletas representados por agentes têm salários melhores e contratos mais duradouros. Confie em nosso trabalho e alcance o sucesso que você merece no mundo do futebol. <br></p>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/sld3.png" class="d-block w-100" alt="imagem3">
+                    <img src="img/player2.png" class="d-none d-xl-block anim-right-left small-secondary-img" id="secondary-img2" alt="imagem3_secundaria">
+                    <h1 class="text-capitalize" id="anim-texto-cima-baixo-slide3">A vitória é garantida para quem se dedica</h1>
+                    <p id="right-left-p-text-slide3" class="right-left-p-text text-light pt-5 d-none d-xl-block slide-in">A FKA está sempre em busca de novos talentos que querem vencer e desejam crescer mais do que seus próprios sonhos. Se você é um atleta que tem essa ambição, então a FKA é o lugar ideal para você. Acreditamos que cada atleta tem um potencial único e procuramos desenvolver esse potencial ao máximo para alcançar grandes feitos nos esportes. Acreditamos que o sucesso vem daqueles que se esforçam e estão determinados a alcançar seus objetivos. Se você é um desses atletas entre em contato conosco.</p>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+        <!-- Slider Fim -->
+
+        <main>
+            <div class="container p-5">
+                <h2>Últimas notícias</h2>
+                <p class="right-left-p-text text-light pt-5">Nenhuma notícia cadastrada até o momento.</p>
+            </div>
+        </main>
+
+        <footer class="bg-dark text-dark pt-5 pb-4">
+            <div class="container">
+
+                <h3 class="text-light">Sobre</h3>
+                <hr class="border border-light border-2 opacity-50">
+                <p class="text-light pt-5 pb-5 h4">
+                    Unindo a nossa experiência no mercado no futebol, a vasta rede de contatos e a habilidade dos jogadores brasileiros, guiamos craques da América Latina para ligas como Premier League, La Liga e outras grandes organizações.
+                </p>
+                <h3 class="text-light">Notícias</h3>
+                <hr class="border border-light border-2 opacity-50">
+                <p class="pt-5 pb-5 h4">
+                    Nenhuma notícia cadastrada até o momento.
+                </p>
+                <h3 class="text-light">Newsletter</h3>
+                <hr class="border border-light border-2 opacity-50">
+                <form>
+                    <div>
+                        <input type="name" class="form-control custom-placeholder pb-3 mb-3 rounded-0" id="name" placeholder="Nome">
+                        <div id="emailHelp" class="form-text"></div>
+                        <input type="email" class="form-control custom-placeholder pb-3 mb-3 rounded-0" id="email" aria-describedby="emailHelp" placeholder="Email">
+                        <div id="emailHelp" class="form-text"></div>
+                    </div>
+                
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-warning btn-outline-dark border-warning custom-placeholder mb-5 rounded-0" type="button">Enviar</button>
+                    </div>
+                </form>
+                                
+            </div>
+            <hr class="border border-light border-1 mt-5">
+            <div class="container">
+                <div class="text-start">
+                    <h5 class="text-light">2023 FKA Sports. All Rights Reserved.</h5>
+                </div>
+            </div>
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var myCarousel = document.getElementById('carouselExampleIndicators');
+                myCarousel.addEventListener('slid.bs.carousel', function () {
+                    var h1Element = document.querySelector('.carousel-item.active #anim-left-right-slide2');
+                    if (h1Element) {
+                        h1Element.classList.add('slide-in');
+                    }
+                    document.querySelectorAll('.right-left-p-text').forEach(function(text) {
+                        text.classList.remove('slide-in');
+                    });
+                    setTimeout(function() {
+                        document.querySelector('.carousel-item.active .right-left-p-text').classList.add('slide-in');
+                    }, 50);
+                });
+            });
+        </script>
+        <script>
+            // Adiciona a classe slide-in após um pequeno atraso para iniciar a animação
+            setTimeout(function() {
+                document.querySelector('.anim-left-right').classList.add('slide-in');
+            }, 100);
+        </script>
+
+        <script>
+            window.onscroll = function() {scrollFunction()};
+
+            function scrollFunction() {
+                var header = document.querySelector('header');
+                var menuNav = document.getElementById('menuNav');
+
+                if (document.body.scrollTop > header.offsetHeight || document.documentElement.scrollTop > header.offsetHeight) {
+                    menuNav.classList.add("fixed-top");
+                } else {
+                    menuNav.classList.remove("fixed-top");
+                }
+            }
+        </script>    
+    </body>
+</html> 
